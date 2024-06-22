99e0b9c49b353a0111ffb66c5a7f23d0181db3e6
Nannan <nanna7077@gmail.com>
2024-06-22 22:45:23 +0530

    Add: About Modal
    Move: Modal to script.js
    
    Fix: Missing Canvas Container
    
    Fix: JS Includes

--- script.js
+++ script.js
@@ -383,3 +383,20 @@ function resetIdleTimer() {
 
 createNewCanvasPage();
 loadFromLocalStorage();
+
+var modal = document.getElementById("centralModal");
+var closeModalBtn = document.getElementsByClassName("close")[0];
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+closeModalBtn.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
